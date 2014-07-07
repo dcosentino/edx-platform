c7b578450ad7463d3ab09e1f7eb48d52fd39411c
marcore <marco.re@polimi.it>
2014-07-07 19:44:18 +0200

    Fix access_token header needed for eco

--- common/djangoapps/third_party_auth/ecobackend.py
+++ common/djangoapps/third_party_auth/ecobackend.py
@@ -1,6 +1,9 @@
 from social.backends.oauth import BaseOAuth2, BaseOAuth1
+from social.backends import google
+import base64
 
-class ECOOpenIdBackend(BaseOAuth2):
+
+class ECOOpenIdBackend(google.GoogleOAuth2):
     """ECOOpendId authentication backend"""
     name = 'ecoopenid-auth'
     REDIRECT_STATE = False
@@ -9,12 +12,26 @@ class ECOOpenIdBackend(BaseOAuth2):
     ACCESS_TOKEN_METHOD = 'POST'
     REVOKE_TOKEN_URL = 'http://ecoidp.test.reimeritsolutions.nl/token'
     REVOKE_TOKEN_METHOD = 'GET'
-    DEFAULT_SCOPE = ['openid']
-    EXTRA_DATA = ['id_token', 'refresh_token']
+    DEFAULT_SCOPE = ['openid','profile','email']
+    EXTRA_DATA = ['access_token','token_type','expires_in','id_token', 'refresh_token']
 
+    def auth_headers(self):
+        return {
+            'Authorization': 'Basic {0}'.format(base64.urlsafe_b64encode(
+                ('{0}:{1}'.format(*self.get_key_and_secret()).encode())
+            ))
+    }
+
+    def get_user_id(self, details, response):
+        """Use sub as unique id"""
+        for p in response:
+            print p,":",response[p]
+        return response['sub']
+    
     def user_data(self, access_token, *args, **kwargs):
         """Return user data from Google API"""
         return self.get_json(
             'http://ecoidp.test.reimeritsolutions.nl/userinfo',
-            params={'access_token': access_token, 'alt': 'json'}
+            headers={'Authorization': 'Bearer {0}'.format(access_token)}
         )
+
