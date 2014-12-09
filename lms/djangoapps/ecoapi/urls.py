ab2b7fd2b9fada4d339074f5c8dad2e467a3273e
Domenico Cosentino <d.cosentino@4sigma.it>
2014-12-09 21:24:02 +0100

    Impostata tabella teacher e relativo invio dati

--- lms/djangoapps/ecoapi/urls.py
+++ lms/djangoapps/ecoapi/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import patterns, url, include
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns(
     '',
-    url(r'^users/(?P<id_user>\d+)/courses',  'webapi.views.user_courses', name='webapi_user_courses'),
-    url(r'^teachers/(?P<id_teacher>\d+)',  'webapi.views.teacher', name='webapi_teacher'),
-    url(r'^heartbeat',  'webapi.views.heartbeat', name='webapi_heartbeat'),
+    url(r'^users/(?P<id_user>\d+)/courses',  'ecoapi.views.user_courses', name='ecoapi_user_courses'),
+    url(r'^teachers/(?P<id_teacher>\d+)',  'ecoapi.views.teacher', name='ecoapi_teacher'),
+    url(r'^heartbeat',  'ecoapi.views.heartbeat', name='ecoapi_heartbeat'),
 )
--- lms/urls.py
+++ lms/urls.py
@@ -571,5 +571,5 @@ urlpatterns += (
 
 # Communication API
 urlpatterns += (
-    url(r'^ecoapi/', include('webapi.urls')),
+    url(r'^ecoapi/', include('ecoapi.urls')),
 )
