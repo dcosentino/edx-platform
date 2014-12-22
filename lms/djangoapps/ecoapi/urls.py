d8c415fb321932e36cd43f484a86d8b25e45b2fe
Marco Re <marco.re@polimi.it>
2014-12-22 11:36:35 +0100

    Cambiata regex per id teachers and users

--- lms/djangoapps/ecoapi/urls.py
+++ lms/djangoapps/ecoapi/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import patterns, url, include
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns(
     '',
-    url(r'^users/(?P<eco_user_id>\w+)/courses',  'ecoapi.views.user_courses', name='ecoapi_user_courses'),
-    url(r'^teachers/(?P<id_teacher>\w+)',  'ecoapi.views.teacher_view', name='ecoapi_teacher'),
+    url(r'^users/(?P<eco_user_id>[^/]+)/courses',  'ecoapi.views.user_courses', name='ecoapi_user_courses'),
+    url(r'^teachers/(?P<id_teacher>[^/]+)',  'ecoapi.views.teacher_view', name='ecoapi_teacher'),
     url(r'^heartbeat',  'ecoapi.views.heartbeat', name='ecoapi_heartbeat'),
 )
